617f2e22dd14c4a3f5caa210334ba2b50593167c
uv <uttamvekariya6338@gmail.com>
2025-02-13 15:16:32 +0530

    complete sort filter

--- src/controller/applicantController.js
+++ src/controller/applicantController.js
@@ -78,6 +78,7 @@ export const viewAllApplicant = async (req, res) => {
       page: pageNum,
       limit: limitNum,
       query,
+      sort: { createdAt: -1 }
     });
 
     res.status(200).json({
