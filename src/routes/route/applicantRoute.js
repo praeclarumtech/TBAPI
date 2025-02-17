cac79cf37d604673292063341c75c960c6323fc4
Uttam vekariya <uttam.praeclarum@gmail.com>
2025-02-17 18:11:09 +0530

    revert change

--- src/routes/route/applicantRoute.js
+++ src/routes/route/applicantRoute.js
@@ -8,14 +8,14 @@ import {
 } from '../../controller/applicantController.js';
 import { applicantValidation } from '../../validations/applicantValidation.js';
 import { validator } from '../../helpers/validator.js';
-// import { authorization } from '../../helpers/user.middleware.js';
+import { authorization } from '../../helpers/user.middleware.js';
 
 const router = express.Router();
 
-router.post('/addApplicant',validator.body(applicantValidation),addApplicant);
-router.get('/viewAllApplicant', viewAllApplicant);
-router.get('/viewApplicant/:id', viewApplicant);
-router.put('/updateApplicant/:id',validator.body(applicantValidation), updateApplicant);
-router.delete('/deleteApplicant/:id', deleteApplicant);
+router.post('/addApplicant',authorization,validator.body(applicantValidation),addApplicant);
+router.get('/viewAllApplicant',authorization, viewAllApplicant);
+router.get('/viewApplicant/:id',authorization, viewApplicant);
+router.put('/updateApplicant/:id',authorization,validator.body(applicantValidation), updateApplicant);
+router.delete('/deleteApplicant/:id',authorization, deleteApplicant);
 
 export default router;
