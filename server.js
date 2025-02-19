781ca7915d62b68ccf2b0faad472ca4c2df8f0c4
Uttam <uttam.praeclarum@gmail.com>
2025-02-19 14:26:42 +0530

    cors add

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ dotenv.config();
 const app = express();
 connectDB();
 
+app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
