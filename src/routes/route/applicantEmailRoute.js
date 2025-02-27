cf71ceb241b77a9dcfbac0e9aaf63eb0569aa1c7
kishan8693 <thakorit104@gmail.com>
2025-02-28 00:12:51 +0530

    add editRoute in emailroutes

--- src/routes/route/applicantEmailRoute.js
+++ src/routes/route/applicantEmailRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { sendEmail,getAllEmails,deleteManyEmails} from '../../controller/applicantEmailController.js';
-import {sendEmailValidation} from '../../validations/sendEMailValidation.js'
+import { sendEmail, getAllEmails, deleteManyEmails } from '../../controller/applicantEmailController.js';
+import { sendEmailValidation } from '../../validations/sendEMailValidation.js'
 import { validator } from '../../helpers/validator.js';
 import { authorization } from '../../helpers/userMiddleware.js';
 const router = express.Router();
 
-router.post('/sendEmail',authorization,validator.body(sendEmailValidation),sendEmail);
-router.get('/getAllEmails',getAllEmails);
-router.delete('/deleteManyEmails',deleteManyEmails);
+router.post('/sendEmail', authorization, validator.body(sendEmailValidation), sendEmail);
+router.post('/editEmail', authorization, validator.body(sendEmailValidation), sendEmail);
+router.get('/getAllEmails', getAllEmails);
+router.delete('/deleteManyEmails', deleteManyEmails);
 export default router;
