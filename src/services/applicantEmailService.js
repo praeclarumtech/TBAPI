5f913b20b1bf653a4fe0a7c99a676276aa0cde0b
Uttam <uttam.praeclarum@gmail.com>
2025-03-11 17:46:24 +0530

    changes in ViewAllemail API

--- src/services/applicantEmailService.js
+++ src/services/applicantEmailService.js
@@ -1,13 +1,47 @@
 import applicantEmail from '../models/applicantEmailModel.js';
 
-export const findAllEmails = async () => {
-  return await applicantEmail.find();
+export const findAllEmails = async (query, page, limit) => {
+  const skip = (page - 1) * limit;
+
+  const emails = await applicantEmail.aggregate([
+    { $match: query },
+    { $unwind: '$email_to' },
+    {
+      $lookup: {
+        from: 'applicants',
+        localField: 'email_to',
+        foreignField: 'email',
+        as: 'applicantDetails',
+      },
+    },
+    {
+      $unwind: { path: '$applicantDetails', preserveNullAndEmptyArrays: true },
+    },
+    {
+      $project: {
+        email_to: 1,
+        email_bcc: 1,
+        subject: 1,
+        description: 1,
+        createdAt: 1,
+        'applicantDetails.name': { $ifNull: ['$applicantDetails.name', ''] },
+        'applicantDetails.appliedSkills': {
+          $ifNull: ['$applicantDetails.appliedSkills', []],
+        },
+      },
+    },
+    { $sort: { createdAt: -1 } },
+    { $skip: skip },
+    { $limit: limit },
+  ]);
+
+  const totalRecords = await applicantEmail.countDocuments(query);
+  return { emails, totalRecords, totalPages: Math.ceil(totalRecords / limit) };
 };
 
 export const removeManyEmails = async (ids) => {
   return await applicantEmail.deleteMany({ _id: { $in: ids } });
 };
-
 
 export const createEmail = async (emailRecords) => {
   try {
