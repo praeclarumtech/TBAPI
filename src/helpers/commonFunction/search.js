4c28ed81a8b5fddbd93d17c037504b10e50c4f3f
Uttam <uttam.praeclarum@gmail.com>
2025-03-17 11:50:45 +0530

    completed applicant search by name and skills

--- src/helpers/commonFunction/search.js
+++ src/helpers/commonFunction/search.js
@@ -1,16 +1,28 @@
 import mongoose from 'mongoose';
 
-export const commonSearch = async (model, searchFields, query, page = 1, limit = 10, sort = { createdAt: -1 }) => {
-  if (!query || typeof query !== 'string') {
+export const commonSearch = async (model, searchFields, query,searchSkills='', page = 1, limit = 10, sort = { createdAt: -1 }) => {
+  if ((!query || typeof query !== 'string') && !searchSkills) {
     return { results: [], totalRecords: 0 };
   }
+  const searchConditions = [];
 
-  const searchConditions = searchFields.map(field => ({
-    [field]: { $regex: query, $options: 'i' }
-  }));
+  if (query) {
+    searchConditions.push(
+      ...searchFields.map(field => ({
+        [field]: { $regex: query, $options: 'i' }
+      }))
+    );
+  }
+
+  if (typeof searchSkills === 'string' && searchSkills.trim().length > 0) {
+    const skillsArray = searchSkills.split(',').map(skill => skill.trim());
+    searchConditions.push({
+      appliedSkills: { $all: skillsArray }
+    });
+  }
 
   const filterQuery = {
-    $or: searchConditions,
+    $or: searchConditions.length > 0 ? searchConditions : [{}],
     isDeleted: false
   };
 
