ab3e970c865522391eb1f673eea7e331692dc9b5
Uttam <uttam.praeclarum@gmail.com>
2025-04-15 12:22:34 +0530

    added authorization in email template API

--- src/routes/route/emailTemplateRoute.js
+++ src/routes/route/emailTemplateRoute.js
@@ -7,14 +7,15 @@ import {
   getAllEmailTemplatesController,
   getEmailTemplateByStatusController
 } from '../../controller/applicantEmailTempletController.js';
+import { authorization } from '../../helpers/userMiddleware.js'
 
 const router = express.Router();
  
-router.post('/createEmailTemplate',  createEmailTemplateController);
-router.get('/viewEmailTemplate/:id', viewEmailTemplateController);
-router.put('/updateEmailTemplate/:id', updateEmailTemplateController);
-router.delete('/deleteEmailTemplate/:id',deleteEmailTemplateController);
-router.get('/getAllEmailTemplates', getAllEmailTemplatesController);
-router.get('/templateByType/:type', getEmailTemplateByStatusController);
+router.post('/createEmailTemplate', authorization, createEmailTemplateController);
+router.get('/viewEmailTemplate/:id', authorization, viewEmailTemplateController);
+router.put('/updateEmailTemplate/:id', authorization, updateEmailTemplateController);
+router.delete('/deleteEmailTemplate/:id', authorization, deleteEmailTemplateController);
+router.get('/getAllEmailTemplates', authorization, getAllEmailTemplatesController);
+router.get('/templateByType/:type', authorization, getEmailTemplateByStatusController);
  
 export default router;
