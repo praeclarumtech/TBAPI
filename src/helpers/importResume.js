4bbecc52228e5876d1d22e272a0fd159b519ff02
Uttam <uttam.praeclarum@gmail.com>
2025-04-24 14:44:59 +0530

    city and state vise applicant count API completed

--- src/helpers/importResume.js
+++ src/helpers/importResume.js
@@ -15,7 +15,9 @@ export const extractTextFromPDF = async (filePath) => {
     const loadingTask = pdfjsLib.getDocument({ data: uint8Array });
     const pdf = await loadingTask.promise;
 
-const pagePromises = Array.from({ length: pdf.numPages }, (_, i) => pdf.getPage(i + 1));
+    const pagePromises = Array.from({ length: pdf.numPages }, (_, i) =>
+      pdf.getPage(i + 1)
+    );
 
     const pages = await Promise.all(pagePromises);
 
@@ -59,18 +61,21 @@ export const extractTextFromDoc = async (filePath) => {
     logger.error(`Failed to extract text from DOC: ${error.message}`);
     return null;
   }
-}
+};
 
 export const parseResumeText = (text) => {
-  const emailRegex = /\b[a-zA-Z0-9._%+-]+(?:\s*@\s*|\s+at\s+)[a-zA-Z0-9.-]+\s*\.\s*[a-zA-Z]{2,}\b/g;
+  const emailRegex =
+    /\b[a-zA-Z0-9._%+-]+(?:\s*@\s*|\s+at\s+)[a-zA-Z0-9.-]+\s*\.\s*[a-zA-Z]{2,}\b/g;
   const phoneRegex = /(\+?\s?91[-\s()]*)?\d{3,5}[-\s()]*\d{3,5}[-\s]*\d{3,5}/g;
-  
+
   const nameTagRegex =
     /(?:Name|Full Name)\s*[:\-]\s*([A-Z][a-z]+(?:\s[A-Z][a-z]+)?(?:\s[A-Z][a-z]+)?)/i;
   const skillsMatch = text.match(/(Technical Skills|Skills)[:\s]*(.*)/i);
   const experienceRegex = /(\d+(?:\.\d+)?)\s*(?:\+?\s*years?|yrs?|year)/gi;
-  const locationRegex = /(Address|Location|Current Address)[:\s]*(\S+(?:\s+\S+){0,5})/i;
-  const preferredLocationRegex = /(Preferred Location)[:\s]*(\S+(?:\s+\S+){0,5})/i;
+  const locationRegex =
+    /(Address|Location|Current Address)[:\s]*(\S+(?:\s+\S+){0,5})/i;
+  const preferredLocationRegex =
+    /(Preferred Location)[:\s]*(\S+(?:\s+\S+){0,5})/i;
   const matches = [...text.matchAll(experienceRegex)].map((match) =>
     parseFloat(match[1])
   );
@@ -91,14 +96,20 @@ export const parseResumeText = (text) => {
   const emailMatch = text.match(emailRegex);
   const email = emailMatch ? emailMatch[0].replace(/\s+/g, '') : '';
   const phoneMatch = text.match(phoneRegex);
-  let phone = phoneMatch ? phoneMatch[0].replace(/[-()\s]/g, '').replace(/^\+91/, '') : '';
+  let phone = phoneMatch
+    ? phoneMatch[0].replace(/[-()\s]/g, '').replace(/^\+91/, '')
+    : '';
   const totalExperience = matches.length > 0 ? Math.max(...matches) : 0;
   const qualification = qualificationMatch
     ? qualificationMatch[1]
     : 'Not Provided';
   const locationMatch = text.match(locationRegex);
-  const currentAddress = locationMatch ? locationMatch[2].trim() : 'Not Provided';
-  const preferredLocations = preferredLocationMatch ? preferredLocationMatch[2].trim() : 'Not Provided';
+  const currentAddress = locationMatch
+    ? locationMatch[2].trim()
+    : 'Not Provided';
+  const preferredLocations = preferredLocationMatch
+    ? preferredLocationMatch[2].trim()
+    : 'Not Provided';
   const linkedinUrl = linkedInMatch ? linkedInMatch[0] : 'Not Found';
   const currentCompanyName = currentCompanyMatch
     ? currentCompanyMatch[2].trim()
@@ -209,30 +220,180 @@ export const parseResumeText = (text) => {
     'Flutter',
     'Figma',
     'adobe XD',
-    'Sketch', 
-   'balsamiq',
+    'Sketch',
+    'balsamiq',
     'Photoshop',
     'illustrator',
     'InVision',
-    'Framer', 
-    'Axure', 
+    'Framer',
+    'Axure',
     'Marvel',
-
   ];
 
   const roleWordList = [
-    "Software Engineer", "Web Designer", "web Developer", "Frontend Developer", "Senior Frontend Developer",
-    "Senior Software Engineer", "Application Development Modernization", "Backend Developer", "Junior Software Engineer",
-    "Full Stack Developer", "Blockchain Developer", "Data Analyst", "Data Scientist", "Product Manager", "Project Engineer",
-    "UI_UX Designer", "Senior UI Engineer", "Application developer", "QA Engineer", "Technical Analyst", "Quality Analyst",
-    "DevOps Engineer", "Business Analyst", "Technical Support Engineer", "Junior MERN Stack Developer", "MERN Stack Developer",
-    "MEAN Stack Developer", "DotNet Developer", "Junior DotNet developer", "Senior DotNet developer", "Java Developer",
-    "Python Developer", "PHP Developer", "Fresher", "Specialist Programmer", "Reactjs Developer", "React Native Developer",
-    "Nodejs Developer", "Senior JavaScript Developer", "Associate Software Engineer", "Associate Process Manager", "Freelancer",
-    "Team Leader", "Senior Angular Developer", "SharePoint Developer", "Placement executive", "System Engineer",
-    "Senior System Engineer", "Programmer Analyst", "Customer Support Specialist", "Other", "Na"
+    'Frontend Architect',
+    'HTML/CSS Developer',
+    'React Native Developer',
+    'Data Engineer',
+    'Azure Developer',
+    'Programmer Analyst',
+    'Interaction Developer',
+    'Java Full Stack Developer',
+    'NET Full Stack Developer',
+    'Junior DotNet Developer',
+    'Data Scientist',
+    'Automation Developer',
+    'Test Engineer',
+    'Senior Frontend Developer',
+    'API Developer',
+    'DotNet Developer',
+    'Web Developer',
+    'Test Lead',
+    'Software Engineer',
+    'Golang Developer',
+    'MERN Stack Developer',
+    'AI Developer',
+    'Cloud Native Developer',
+    'Systems Software Developer',
+    'Quality Analyst',
+    'Lead Software Engineer',
+    'UI Developer',
+    'Backend Developer',
+    'GCP Developer',
+    'CRM Developer',
+    'QA Engineer',
+    'Application Developer',
+    'System Engineer',
+    'Senior Angular Developer',
+    'Python Backend Developer',
+    'WordPress Developer',
+    'Embedded Software Developer',
+    'Automation Test Engineer',
+    'Software Test Engineer',
+    'Specialist Programmer',
+    'Machine Learning Engineer',
+    'Other',
+    'Node.js Developer',
+    'Java Developer',
+    'iOS Developer',
+    'Mobile App Developer',
+    'Data Analyst',
+    'Unity Developer',
+    'Magento Developer',
+    'Test Architect',
+    'Fresher',
+    'Senior Software Engineer',
+    'Frontend Developer',
+    'LAMP Stack Developer',
+    'PHP Developer',
+    'DevOps Engineer',
+    'Drupal Developer',
+    'API Integration Developer',
+    'Software Developer',
+    'Associate Software Engineer',
+    'Java Backend Developer',
+    'Security Test Engineer',
+    'Vue.js Developer',
+    'Ruby on Rails Developer',
+    'Backend Architect',
+    'Flutter Developer',
+    'Chatbot Developer',
+    'Manual Test Engineer',
+    'Full Stack Developer',
+    'Junior MERN Stack Developer',
+    'Python Developer',
+    'ETL Developer',
+    'Senior System Engineer',
+    'JavaScript Developer',
+    'React.js Developer',
+    'UX Engineer',
+    'Frontend UI/UX Developer',
+    'Cloud Developer',
+    'Platform Engineer',
+    'Application Development Modernization',
+    'Web UI Engineer',
+    'C# Backend Developer',
+    'PHP Full Stack Developer',
+    'Xamarin Developer',
+    'AWS Developer',
+    'Game Developer',
+    'AR Developer',
+    'Blockchain Developer',
+    'SharePoint Developer',
+    'ERP Developer',
+    'Salesforce Developer',
+    'Angular Developer',
+    'MEAN Stack Developer',
+    'Senior DotNet Developer',
+    'Unreal Engine Developer',
+    'VR Developer',
+    'Web Application Developer',
+    'CMS Developer',
+    'Shopify Developer',
+    'QA Automation Engineer',
+    'Performance Test Engineer',
+    'UI Engineer',
+    'Android Developer',
+    'Mobile UI Developer',
+    'Big Data Developer',
+    'Junior PHP Developer',
+    'Associate PHP Developer',
+    'Senior PHP Developer',
+    'Laravel Developer',
+    'Junior LAMP Stack Developer',
+    'Symfony Developer',
+    'CodeIgniter Developer',
+    'Drupal PHP Developer',
+    'WordPress PHP Developer',
+    'Magento PHP Developer',
+    'PHP Software Engineer',
+    'Associate LAMP Stack Developer',
+    'Senior LAMP Stack Developer',
+    'PHP Architect',
+    'Junior Software Engineer',
+    'PHP Engineer',
+    'PHP Backend Developer',
+    'PHP API Developer',
+    'PHP Web Developer',
+    'Senior PHP API Developer',
+    'PHP DevOps Engineer',
+    'Junior Frontend Developer',
+    'Associate Frontend Developer',
+    'Junior React.js Developer',
+    'Associate React.js Developer',
+    'Senior React.js Developer',
+    'Junior Angular Developer',
+    'Associate Angular Developer',
+    'Junior Backend Developer',
+    'Associate Backend Developer',
+    'Senior Backend Developer',
+    'Junior Node.js Developer',
+    'Associate Node.js Developer',
+    'Senior Node.js Developer',
+    'Junior Java Developer',
+    'Associate Java Developer',
+    'Senior Java Developer',
+    'Junior Python Developer',
+    'Associate Python Developer',
+    'Senior Python Developer',
+    'Junior C# Backend Developer',
+    'Associate C# Backend Developer',
+    'Senior C# Backend Developer',
+    'Junior Full Stack Developer',
+    'Associate Full Stack Developer',
+    'Senior Full Stack Developer',
+    'Associate MERN Stack Developer',
+    'Senior MERN Stack Developer',
+    'Junior MEAN Stack Developer',
+    'Associate MEAN Stack Developer',
+    'Senior MEAN Stack Developer',
+    'Associate DotNet Developer',
+    'Junior WooCommerce Developer',
+    'Associate WooCommerce Developer',
+    'WooCommerce Developer',
+    'Senior WooCommerce Developer',
+    'Senior Web Developer',
   ];
-  
 
   const escapeRegex = (word) => word.replace(/[.*?^${}()|[\]\\#+]/g, '\\$&');
 
@@ -328,44 +489,41 @@ export const parseResumeText = (text) => {
   const lastName =
     nameParts.length > 2 ? nameParts[2] : nameParts[1] || 'Unknown';
 
-    let finalGender = null;
- 
-    if (genderMatch && genderMatch[0]) {
-      const found = genderMatch[0].toLowerCase();
-     
-      if (['male'].includes(found)) {
-        finalGender = 'male';
-      }
-     
-      if (['female', 'famale', 'femail'].includes(found)) {
-        finalGender = 'female';
-      }
+  let finalGender = null;
+
+  if (genderMatch && genderMatch[0]) {
+    const found = genderMatch[0].toLowerCase();
+
+    if (['male'].includes(found)) {
+      finalGender = 'male';
     }
 
-    let obj = {
-      name: {
-        firstName,
-        middleName,
-        lastName,
-      },
-      email,
-      phone: {
-        phoneNumber: phone,
-        whatsappNumber: phone,
-      },
-      otherSkills: skills.join(', '),
-      totalExperience,
-      qualification,
-      appliedRole,
-      currentAddress,
-      linkedinUrl,
-      currentCompanyName,
-      preferredLocations,
-      maritalStatus,
-      dateOfBirth,
+    if (['female', 'famale', 'femail'].includes(found)) {
+      finalGender = 'female';
     }
-    if (finalGender !== null)  obj.gender = finalGender
-    return obj
+  }
+
+  return {
+    name: {
+      firstName,
+      middleName,
+      lastName,
+    },
+    email,
+    phone: {
+      phoneNumber: phone,
+      whatsappNumber: phone,
+    },
+    otherSkills: skills.join(', '),
+    totalExperience,
+    qualification,
+    appliedRole,
+    currentAddress,
+    linkedinUrl,
+    currentCompanyName,
+    preferredLocations,
+    maritalStatus,
+    dateOfBirth,
+    gender: finalGender,
+  };
 };
-
-
