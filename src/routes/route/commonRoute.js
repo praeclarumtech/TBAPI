5588b196b21d41c9ad505bef4e9148c2d855c8a3
RaviPraeclarum <ravi@praeclarumtech.com>
2025-07-24 16:50:32 +0530

    Revert "Development"

--- src/routes/route/commonRoute.js
+++ src/routes/route/commonRoute.js
@@ -3,29 +3,29 @@ import {
     viewCountry, viewState, viewCity, viewCountryById, addCountry, updateCountryById, deleteCountryById, deleteManyCountries,
     addState, updateStateById, deleteManyStates, deleteStateById, viewStateById, viewCityById, updateCityById, deleteCityById, deleteManyCities, addCity
 } from '../../controller/commonController.js';
-import { authorization } from '../../helpers/userMiddleware.js';
+import { authorization, verifyRoles } from '../../helpers/userMiddleware.js';
 
 const router = express.Router();
 
-router.get('/country', authorization,  viewCountry);
-router.get('/viewCountryById/:id', authorization,  viewCountryById);
-router.post('/addCountry', authorization,  addCountry);
-router.put('/updateCountry/:countryId', authorization,  updateCountryById);
-router.delete('/deleteCountry/:id', authorization,  deleteCountryById);
-router.delete('/deleteManyCountry', authorization,  deleteManyCountries);
+router.get('/country', authorization, verifyRoles(['admin', 'hr','vendor','guest','client']), viewCountry);
+router.get('/viewCountryById/:id', authorization, verifyRoles(['admin', 'hr']), viewCountryById);
+router.post('/addCountry', authorization, verifyRoles(['admin', 'hr']), addCountry);
+router.put('/updateCountry/:countryId', authorization, verifyRoles(['admin']), updateCountryById);
+router.delete('/deleteCountry/:id', authorization, verifyRoles(['admin']), deleteCountryById);
+router.delete('/deleteManyCountry', authorization, verifyRoles(['admin']), deleteManyCountries);
 
-router.get('/state', authorization,  viewState);
-router.post('/addState', authorization,  addState);
-router.put('/updateState/:stateId', authorization,  updateStateById);
-router.delete('/deleteState/:id', authorization,  deleteStateById);
-router.delete('/deleteManyState', authorization,  deleteManyStates);
-router.get('/viewStateById/:id', authorization, viewStateById);
+router.get('/state', authorization, verifyRoles(['admin', 'hr','client','vendor']), viewState);
+router.post('/addState', authorization, verifyRoles(['admin', 'hr']), addState);
+router.put('/updateState/:stateId', authorization, verifyRoles(['admin']), updateStateById);
+router.delete('/deleteState/:id', authorization, verifyRoles(['admin']), deleteStateById);
+router.delete('/deleteManyState', authorization, verifyRoles(['admin']), deleteManyStates);
+router.get('/viewStateById/:id', authorization, verifyRoles(['admin', 'hr']), viewStateById);
 
-router.get('/city', authorization, viewCity);
-router.post('/addCity', authorization, addCity);
-router.get("/viewCityById/:id", authorization,  viewCityById);
-router.put("/updateCityById/:city_id", authorization,  updateCityById);
-router.delete("/deleteCityById/:id", authorization,  deleteCityById);
-router.delete("/deleteManyCities", authorization,  deleteManyCities);
+router.get('/city', authorization, verifyRoles(['admin', 'hr', 'client', 'vendor', 'guest']), viewCity);
+router.post('/addCity', authorization, verifyRoles(['admin', 'hr']), addCity);
+router.get("/viewCityById/:id", authorization, verifyRoles(['admin', 'hr']), viewCityById);
+router.put("/updateCityById/:city_id", authorization, verifyRoles(['admin']), updateCityById);
+router.delete("/deleteCityById/:id", authorization, verifyRoles(['admin']), deleteCityById);
+router.delete("/deleteManyCities", authorization, verifyRoles(['admin']), deleteManyCities);
 
 export default router;
