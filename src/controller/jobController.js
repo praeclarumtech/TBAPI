11d4e54646386a07d04798e39b9e7f109d6d3d27
ashvin <ashvinomprakashz24@gmail.com>
2025-09-09 12:39:21 +0530

    export update

--- src/controller/jobController.js
+++ src/controller/jobController.js
@@ -15,9 +15,8 @@ import { generateJobId } from '../helpers/generateApplicationNo.js';
 import { getAllusers, getUser } from '../services/userService.js';
 import { Enum } from '../utils/enum.js';
 import User from '../models/userModel.js';
-import { jobCreatedTemplate } from '../emailTemplates/jobCreatedTemplate.js';
-import { sendingEmail } from '../helpers/sendEmail.js';
-
+import { sendingEmail } from "../utils/email.js";
+import {jobCreatedTemplate} from "../utils/emailTemplates/emailTemplates.js";
 
 export const createJob = async (req, res) => {
   try {
