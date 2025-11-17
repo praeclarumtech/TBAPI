105f0851cf6e9b03716ce69e72c1150103dff198
sonalRathod024 <sonal@praeclarumtech.com>
2025-11-17 19:08:50 +0530

    fix env issue

--- src/helpers/commonFunction/handleEmail.js
+++ src/helpers/commonFunction/handleEmail.js
@@ -18,6 +18,9 @@ export const sendingEmail = async ({
     auth: {
       user: process.env.USER,
       pass: process.env.PASS,
+      host: process.env.HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
     },
   });
 
